26e1837aa5bad48c72f478218037d47971a14370
Papimar <deadzoneoficial666@gmail.com>
2025-09-05 21:02:14 -0600

    First commit

---
+++ MicroscoposAnimae/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_MicroscoposAnimae_FILE D:/MicroscoposAnimae/MicroscoposAnimae/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/MicroscoposAnimae.exe)
+set(__QT_DEPLOY_TARGET_MicroscoposAnimae_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_MicroscoposAnimae_RUNTIME_DLLS D:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
---
+++ MicroscoposAnimae/main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "MicroscoposAnimae_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    printf("pitito de burro");
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
